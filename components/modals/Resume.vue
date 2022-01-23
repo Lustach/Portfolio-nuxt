dde12d0ee4126e64523539c204540ef286a44f82
Lustach <lusta.vlad2001@gmail.com>
2022-01-23 15:58:33 +0300

    UPD

--- components/modals/Resume.vue
+++ components/modals/Resume.vue
@@ -20,70 +20,17 @@
             <h4 class="timeline__title">
               Коммерческий опыт
             </h4>
-
-            <div class="timeline__item">
-              <time class="timeline__date" datetime="11-21-2019">2021 - <b>2022</b></time>
+            <div v-for="(exp,key) in experienceList" :key="key" class="timeline__item">
+              <time class="timeline__date" :datetime="exp.date_start">{{ exp.date_start }} - <b>{{ exp.date_end }}</b></time>
               <h5 class="timeline__subtitle">
-                Trinity Group
+                {{ exp.company }}
               </h5>
               <div class="timeline__position">
-                Vue Front-end разработчик
+                {{ exp.position }}
               </div>
               <div class="timeline__text">
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis debitis autem aspernatur sed iusto maiores nihil magni provident dolore, animi nemo est
-                  corporis in
-                  facilis eum quae sint aliquam, ipsam?
-                </p>
-              </div>
-            </div>
-
-            <div class="timeline__item">
-              <time class="timeline__date" datetime="11-21-2019">2020- <b>2021</b></time>
-              <h5 class="timeline__subtitle">
-                CraftGroup
-              </h5>
-              <div class="timeline__position">
-                Vue Front-end разработчик
-              </div>
-              <div class="timeline__text">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis debitis autem aspernatur sed iusto maiores nihil magni provident dolore, animi nemo est
-                  corporis in
-                  facilis eum quae sint aliquam, ipsam?
-                </p>
-              </div>
-            </div>
-
-            <div class="timeline__item">
-              <time class="timeline__date" datetime="11-21-2019">2020 - <b>2021</b></time>
-              <h5 class="timeline__subtitle">
-                Фрилансер
-              </h5>
-              <div class="timeline__position">
-                Front-end разработчик
-              </div>
-              <div class="timeline__text">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis debitis autem aspernatur sed iusto maiores nihil magni provident dolore, animi nemo est
-                  corporis in
-                  facilis eum quae sint aliquam, ipsam?
-                </p>
-              </div>
-            </div>
-            <div class="timeline__item">
-              <time class="timeline__date" datetime="11-21-2019">2020 - <b>2021</b></time>
-              <h5 class="timeline__subtitle">
-                Фрилансер
-              </h5>
-              <div class="timeline__position">
-                Front-end разработчик
-              </div>
-              <div class="timeline__text">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis debitis autem aspernatur sed iusto maiores nihil magni provident dolore, animi nemo est
-                  corporis in
-                  facilis eum quae sint aliquam, ipsam?
+                  {{ exp.description }}
                 </p>
               </div>
             </div>
@@ -103,93 +50,63 @@
                 ДонНТУ г. Донецк
               </div>
             </div>
-
-            <!--            <div class="timeline__item">-->
-            <!--              <time class="timeline__date" datetime="11-21-2019">2005 - 2010</time>-->
-            <!--              <h5 class="timeline__subtitle">UI/UX Design</h5>-->
-            <!--              <div class="timeline__position">LA University, USA</div>-->
-            <!--            </div>-->
-
             <h4 class="timeline__title">
               Навыки:
             </h4>
+
             <div class="progress-bar">
               <h3 class="">
                 Hard:
               </h3>
-              <div class="progress-bar__item">
-                <div class="progress-bar__left">
-                  <div class="progress-bar__title">
-                    JavaScript
+              <div class="progress-bar__container">
+                <div class="progress-bar__item" style="display: flex;    flex-direction: column;    align-items: baseline;">
+                  <div v-for="(skill,key) in skillList.hard" :key="key" class="progress-bar__left">
+                    <div class="progress-bar__title">
+                      {{ skill.name }}
+                    </div>
                   </div>
                 </div>
-                <div class="progress-bar__right">
-                  <div class="progress-bar__bar">
-                    <div class="progress-bar__progress" style="width: 90%;" />
-                  </div>
-                </div>
-              </div>
-
-              <div class="progress-bar__item">
-                <div class="progress-bar__left">
-                  <div class="progress-bar__title">
-                    HTML/CSS
-                  </div>
-                </div>
-                <div class="progress-bar__right">
-                  <div class="progress-bar__bar">
-                    <div class="progress-bar__progress" style="width: 95%;" />
+                <div class="progress-bar__progress__container" style="padding-left: 1.2rem;">
+                  <div v-for="(skill,key) in skillList.hard" :key="key" class="progress-bar__item">
+                    <div class="progress-bar__right">
+                      <div class="progress-bar__bar">
+                        <div class="progress-bar__progress" :style="{width: skill.progress + '%'}"/>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-
-              <div class="progress-bar__item">
-                <div class="progress-bar__left">
-                  <div class="progress-bar__title">
-                    TypeScript
+              <h3 class="">
+                Soft:
+              </h3>
+              <div class="progress-bar__container">
+                <div class="progress-bar__item" style="display: flex;    flex-direction: column;    align-items: baseline;">
+                  <div v-for="(skill,key) in skillList.soft" :key="key" class="progress-bar__left">
+                    <div class="progress-bar__title">
+                      {{ skill.name }}
+                    </div>
                   </div>
                 </div>
-                <div class="progress-bar__right">
-                  <div class="progress-bar__bar">
-                    <div class="progress-bar__progress" style="width: 85%;" />
-                  </div>
-                </div>
-              </div>
-              <div class="progress-bar__item">
-                <div class="progress-bar__left">
-                  <div class="progress-bar__title">
-                    Vue/Vuex
-                  </div>
-                </div>
-                <div class="progress-bar__right">
-                  <div class="progress-bar__bar">
-                    <div class="progress-bar__progress" style="width: 95%;" />
+                <div class="progress-bar__progress__container" style="padding-left: 1.2rem;">
+                  <div v-for="(skill,key) in skillList.soft" :key="key" class="progress-bar__item">
+                    <div class="progress-bar__right">
+                      <div class="progress-bar__bar">
+                        <div class="progress-bar__progress" :style="{width: skill.progress + '%'}"/>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div><!-- /.progress-bar -->
-
-            <!--            <h4 class="timeline__title">Awards</h4>-->
-
-            <!--            <div class="timeline__item">-->
-            <!--              <time class="timeline__date" datetime="11-21-2019">2010</time>-->
-            <!--              <h5 class="timeline__subtitle">UI/UX Design</h5>-->
-            <!--              <div class="timeline__position">LA University, USA</div>-->
-            <!--              <div class="timeline__text">-->
-            <!--                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis debitis autem aspernatur sed iusto maiores nihil magni provident dolore, animi nemo est-->
-            <!--                  corporis in-->
-            <!--                  facilis eum quae sint aliquam, ipsam?</p>-->
-            <!--              </div>-->
-            <!--            </div>-->
-          </div><!-- /.timeline__col -->
-        </div><!-- /.timeline -->
-
+            </div>
+          </div>
+        </div><!-- /.timeline__col -->
         <div class="text-center">
           <a class="btn" href="#" data-modal="#modal_hire_me" @click="showHireMe()">Нанять</a>
         </div>
-      </div><!-- /.modal__content -->
-    </div><!-- /.modal__dialog -->
-  </div>
+      </div><!-- /.timeline -->
+
+    </div><!-- /.modal__content -->
+  </div><!-- /.modal__dialog -->
 </template>
 
 <script>
@@ -199,38 +116,81 @@ export default {
   name: 'Resume',
   mixins: [modal],
   data: () => ({
-    experienceList: [{
-      date_start: '2021',
-      date_end: '2022',
-      company: 'Trinity Group',
-      position: 'Vue Front-end разработчик',
-      description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis debitis autem aspernatur sed iusto maiores nihil magni provident dolore, animi nemo est
+    experienceList: [
+      {
+        date_start: '2021',
+        date_end: '2022',
+        company: 'Trinity Group',
+        position: 'Vue Front-end разработчик',
+        description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis debitis autem aspernatur sed iusto maiores nihil magni provident dolore, animi nemo est
                   corporis in facilis eum quae sint aliquam, ipsam?`
-    }, {
-      date_start: '2020',
-      date_end: '2021',
-      company: 'CraftGroup',
-      position: 'Vue Front-end разработчик',
-      description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis debitis autem aspernatur sed iusto maiores nihil magni provident dolore, animi nemo est
+      }, {
+        date_start: '2020',
+        date_end: '2021',
+        company: 'CraftGroup',
+        position: 'Vue Front-end разработчик',
+        description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis debitis autem aspernatur sed iusto maiores nihil magni provident dolore, animi nemo est
                   corporis in
                   facilis eum quae sint aliquam, ipsam?`
-    }, {
-      date_start: '2020',
-      date_end: '2021',
-      company: 'Фрилансер',
-      position: 'Front-end разработчик',
-      description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis debitis autem aspernatur sed iusto maiores nihil magni provident dolore, animi nemo est
+      }, {
+        date_start: '2020',
+        date_end: '2021',
+        company: 'Фрилансер',
+        position: 'Front-end разработчик',
+        description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis debitis autem aspernatur sed iusto maiores nihil magni provident dolore, animi nemo est
                   corporis in
                   facilis eum quae sint aliquam, ipsam?`
-    }, {
-      date_start: '2019',
-      date_end: '2020',
-      company: 'Астелит',
-      position: 'Vue Front-end разработчик',
-      description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis debitis autem aspernatur sed iusto maiores nihil magni provident dolore, animi nemo est
+      }, {
+        date_start: '2019',
+        date_end: '2020',
+        company: 'Астелит',
+        position: 'Vue Front-end разработчик',
+        description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis debitis autem aspernatur sed iusto maiores nihil magni provident dolore, animi nemo est
                   corporis in
                   facilis eum quae sint aliquam, ipsam?`
-    }]
+      }],
+    skillList: {
+      hard: [
+        {
+          name: 'JavaScript',
+          progress: '90'
+        },
+        {
+          name: 'HTML/CSS',
+          progress: '95'
+        },
+        {
+          name: 'TypeScript',
+          progress: '85'
+        },
+        {
+          name: 'Vue',
+          progress: '95'
+        }
+      ],
+      soft: [
+        {
+          name: 'Коммуницирование',
+          progress: '99'
+        },
+        {
+          name: 'Стрессоустойчивость',
+          progress: '95'
+        },
+        {
+          name: 'Дисциплинированность',
+          progress: '99'
+        },
+        {
+          name: 'Ответственность',
+          progress: '95'
+        },
+        {
+          name: 'Усидчивость',
+          progress: 90
+        }
+      ]
+    }
   })
 }
 </script>
