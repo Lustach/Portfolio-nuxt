5e54cc23219d928443eb76e48056dddb8c66bc17
Lustach <lusta.vlad2001@gmail.com>
2023-09-19 11:56:57 +0400

    upd

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,46 +6,46 @@ export default {
   // },
   // generate: { fallback: true },
   head: {
-    title: 'Lusta Vlad Portfolio',
+    title: "Lusta Vlad Portfolio",
     // <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,300;0,400;0,700;1,300&display=swap" rel="stylesheet">
     //   <link rel="stylesheet" href="css/style.min.css">
     //     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     //       <link rel="shortcut icon" href="https://toppng.com/uploads/preview/krasivye-logotipy-115507292386wws3ynfmr.png" type="image/x-icon">
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
+      { charset: "utf-8", },
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
       },
       {
-        hid: 'description',
-        name: 'description',
-        content: ''
+        hid: "description",
+        name: "description",
+        content: "",
       },
       {
-        name: 'format-detection',
-        content: 'telephone=no'
-      }
+        name: "format-detection",
+        content: "telephone=no",
+      },
     ],
-    link: [{
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon.ico'
-    }]
+    link: [ {
+      rel: "icon",
+      type: "image/x-icon",
+      href: "/favicon.ico",
+    }, ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~/assets/scss/style.scss'],
+  css: [ "~/assets/scss/style.scss", ],
   server: {
-    port: process.env.PORT
+    port: process.env.PORT,
   },
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/axios',
-    '~/plugins/vee-validate/vee-validate'
+    "~/plugins/axios",
+    "~/plugins/vee-validate/vee-validate",
   ],
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -54,27 +54,27 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build'
+    "@nuxt/typescript-build",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    "@nuxtjs/axios",
     // '~/plugins/axios'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: process.env.BASE_URL
+    baseURL: process.env.BASE_URL,
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     extractCSS: true,
     transpile: [
-      'vee-validate'
-    ]
+      "vee-validate",
+    ],
   },
 
   render: {
@@ -93,6 +93,6 @@ export default {
       //   'style-src': ["'self'", "'unsafe-inline'"],
       //   'connect-src': ["'self'", 'https://mc.yandex.ru']
       // }
-    }
-  }
-}
+    },
+  },
+};
